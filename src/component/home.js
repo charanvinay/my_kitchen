eaff2db359640f0c0117eb09933f1ea068d7cbbe
charanvinay_ael <charanvinay@auroraelabs.com>
2023-02-01 14:05:30 +0530

    test 3

--- src/component/home.js
+++ src/component/home.js
@@ -4,7 +4,7 @@ const Home = () => {
 
   console.log(`${process.env.REACT_APP_API_KEY}`)
   return (
-    <div>Home {process.env.REACT_APP_API_KEY}</div>
+    <div>Home {process.env.REACT_APP_API_KEY} <br/>REACT_APP_AUTH_DOMAIN = {process.env.REACT_APP_AUTH_DOMAIN}</div>
   )
 }
 
