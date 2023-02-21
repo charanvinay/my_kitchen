c6e060477f01cadabe7a480ba0449d295eb69381
charanvinay_ael <charanvinay@auroraelabs.com>
2023-02-21 21:21:07 +0530

    removed imgcapture

--- src/component/Recipe/finish.js
+++ src/component/Recipe/finish.js
@@ -33,7 +33,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
 import { bottomButtonsStyle } from "../../Common/Constants";
 import ErrorAlert from "../../Common/ErrorAlert";
-import ImgCapture from "../../Common/ImgCapture";
 import ImgWithLabelCard from "../../Common/ImgWithLabelCard";
 import CKeditor from "../../Common/Skeletons/CKeditor";
 import Step from "../../Common/Skeletons/Step";
@@ -322,7 +321,6 @@ const Finish = (props) => {
               </Button>
             </Stack>
           </Box>
-          <ImgCapture />
           <ErrorAlert
             snackopen={snackopen}
             handleClose={handleCloseSnackbar}
