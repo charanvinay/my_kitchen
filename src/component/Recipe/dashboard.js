570fc787817f262e25908986d3e19315ed5284a0
charanvinay_ael <charanvinay@auroraelabs.com>
2023-03-08 13:14:47 +0530

    search bar in dashboard

--- src/component/Recipe/dashboard.js
+++ src/component/Recipe/dashboard.js
@@ -1,19 +1,31 @@
-import { Box, Container, Grid, Stack, Typography } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  IconButton,
+  InputBase,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { grey } from "@mui/material/colors";
-import { collection, collectionGroup, endAt, getDocs, orderBy, query, startAt, where } from "firebase/firestore";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import NoData from "../../Assets/no_data_found.svg";
-import HeadingLG from "../../Common/HeadingLG";
 import HeadingLGBlue from "../../Common/HeadingLGBlue";
 import RecipeCardSkeleton from "../../Common/Skeletons/RecipeCard";
 import { auth, db } from "../../services/firebase";
 import RecipeCard from "./recipe_card";
+import FilterListIcon from "@mui/icons-material/FilterList";
 
 const Dashboard = () => {
   const [user, loading] = useAuthState(auth);
   const [recipesList, setRecipesList] = useState([]);
   const [loadding, setLoadding] = useState(true);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     if (loading) {
@@ -25,17 +37,16 @@ const Dashboard = () => {
   }, [user, loading]);
 
   const getUserRecipes = async () => {
-    var searchString = ""
     setRecipesList([]);
     setLoadding(true);
     try {
       let user_ref = query(
         collection(db, "recipes"),
         orderBy("title"),
-        where("title", ">=", searchString),
-        where("title", "<=", searchString + "\uf8ff")
-        
-        // endAt(searchString + '\uf8ff')
+        where("title", ">=", searchText),
+        where("title", "<=", searchText + "\uf8ff")
+
+        // endAt(searchText + '\uf8ff')
         // where("uid", "==", user?.uid)
       );
       let user_docs = await getDocs(user_ref);
@@ -54,8 +65,43 @@ const Dashboard = () => {
     }
   };
 
+  useEffect(() => {
+    getUserRecipes();
+  }, [searchText]);
+
   return (
     <Container maxWidth="lg" sx={{ p: 3 }}>
+      <HeadingLGBlue text1="Explore" text2="Recipes" />
+      <Stack spacing={2} sx={{ marginY: 2 }}>
+        <Paper
+          component="form"
+          elevation={0}
+          sx={{
+            p: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Search Recipe"
+            inputProps={{ "aria-label": "search recipe" }}
+            value={searchText}
+            onChange={(e) => {
+              console.log(e.target.value);
+              setSearchText(e.target.value);
+            }}
+          />
+          <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+        <Stack>
+          <Box sx={{ display: "flex", justifyContent: "flex-end", alignItems: "center" }}>
+            <Button sx={{alignItems: "start"}} startIcon={<FilterListIcon />}>Filter</Button>
+          </Box>
+        </Stack>
+      </Stack>
       {loadding && (
         <Grid container spacing={{ xs: 6, md: 3 }}>
           {[1, 2, 3, 4, 5, 6].map((loader) => (
@@ -67,8 +113,7 @@ const Dashboard = () => {
       )}
       {!loadding && recipesList.length > 0 ? (
         <Box sx={{ marginTop: 2 }}>
-          <HeadingLGBlue text1="Explore" text2="Recipes" />
-          <Grid container spacing={3} sx={{ marginTop: 2 }}>
+          <Grid container spacing={3}>
             {recipesList?.map((recipe) => {
               return (
                 <Grid item xs={12} md={4} key={recipe._id}>
